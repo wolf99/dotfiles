e8efb3a58131f7d400d1cca85a29964d65862639
wolf99 <281700+wolf99@users.noreply.github.com>
2020-05-23 00:17:05 +0100

    Add create-links.sh

---
+++ create-links.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+# ## Description
+#
+# Creates a standard set of links for my WSL setup
+# Links are created in the directory the script is run from
+#
+# ## Arguments
+#
+# - First argument should be the Windows username in the form it appears
+#    in at %HOMEDRIVE%%HOMEPATH%
+#
+# ## Example Usage
+#
+# ```bash
+# ./create-links.sh wolf99
+# ```
+#
+
+ln -s /mnt/c/Users/"$1"/OneDrive/Documents/development dev
+ln -s /mnt/c/Users/"$1"/git git
+ln -s /mnt/c/Users/"$1"/OneDrive one
